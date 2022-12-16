0f34b355360df0469df6b55922f3302632739a14
macster9 <thomas.mcrobie999@gmail.com>
2022-12-16 16:24:42 +0000

    added queries file structure worker around warnings

---
+++ data_pipeline/queries.py
@@ -0,0 +1,8 @@
+# from marvin.tools import Cube, Maps, Image
+# from marvin.tools.query import Query
+import os
+from marvin import config
+
+def query_single():
+    os.system("cls")
+    print(config.release)
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-import torch
-import numpy
+import matplotlib.pyplot as plt
+import os
+
+if __name__ == "__main__":
+
+    os.environ['HOME'] = 'C:/Users/thoma/marvin'
+    os.environ['SAS_BASE_DIR'] = os.path.join(os.getenv("HOME"), 'sas')
+    print("Ignore following __warnings__:")
+    plt.pause(0.1)
+    from data_pipeline.queries import query_single
+    query_single()
